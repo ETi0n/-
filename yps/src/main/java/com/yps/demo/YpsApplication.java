c63502e9b4c20c6526c046cf433d3edcd8516558
ETi0n <ysl04030@gmail.com>
2024-11-26 12:43:38 +0900

    초기세팅

---
+++ yps/src/main/java/com/yps/demo/YpsApplication.java
@@ -0,0 +1,13 @@
+package com.yps.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YpsApplication.class, args);
+	}
+
+}
---
+++ yps/src/main/java/com/yps/demo/service/MemberService.java
@@ -0,0 +1,8 @@
+package com.yps.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+    
+}
